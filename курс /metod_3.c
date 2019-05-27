e40befec9af50780dca34ffe1a62e9a9aa266945
Natavrik <44323332+Natavrik@users.noreply.github.com>
2019-05-27 17:25:08 +0300

    Update metod_3.c

--- курс /metod_3.c
+++ курс /metod_3.c
@@ -1,5 +1,92 @@
 #include <stdio.h> 
 #include <math.h> 
+#define epsilon 0.000000001
+double f(double, double , double ); 
+
+int main ( ) 
+{ 
+double a, b, otv=0, y, c, d; 
+int k=0;
+printf ("введите интервал от a до b \n"); 
+printf ("введите a : "); 
+scanf ("%lf", &a); 
+printf ("введите b: "); 
+scanf ("%lf", &b); 
+//printf ("введите точность вычислений: "); 
+//scanf ("%lf", &epsilon);
+printf ("введите с(констатнта при икс): "); 
+scanf ("%lf", &c); 
+printf ("введите значение положения функции по y : "); 
+scanf ("%lf", &d);
+printf ("\n\n\nКоличество интераций и значение по х\n\n\n"); 
+printf ("________________\n\n");
+
+do { 
+y=otv; 
+otv=b-((b-a)/(f(b,c,d)-f(a,c,d)))*f(b,c,d); 
+a=b; 
+b=otv; 
+k++;
+printf("%d) %lf\n\n", k, otv);
+// printf ("Ответ: %lf\n Количество интераций %d\n", otv, k); 
+} 
+while (fabs(y-otv)>=epsilon); 
+printf ("________________\n"); 
+// printf ("Ответ: %lf\n %d/n", otv, k); 
+return 0; 
+} 
+
+double f(double x, double c, double d) 
+{ 
+return sin(c*x)-d; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+______________________________________________________________
+______________________________________________________________
+______________________________________________________________
+
+
+
+
+
+
+#include <stdio.h> 
+#include <math.h> 
  
 double f(double, double , double ); 
  
